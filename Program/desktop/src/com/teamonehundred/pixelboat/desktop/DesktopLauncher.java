f269a5b223f4d626c71e6a23f9b284295268e50f
wpw503 <wpw503@york.ac.uk>
2020-10-26 16:58:24 +0000

    initial program structure

---
+++ Program/desktop/src/com/teamonehundred/pixelboat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teamonehundred.pixelboat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teamonehundred.pixelboat.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
