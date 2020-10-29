53796e0f075a2ec21d6d5f4541d713496566aa20
Fritzbox2000 <woonters@gmail.com>
2020-10-29 14:23:47 +0000

    Create class for boat AI, (TODO: Learn Java)

---
+++ Implementation/core/src/com/teamonehundred/pixelboat/AIBoat.java
@@ -0,0 +1,16 @@
+package com.teamonehundred.pixelboat;
+
+public class AIBoat extends Boat{
+    /* ################################### //
+                   ATTRIBUTES
+    // ################################### */
+
+    /* ################################### //
+              CONSTRUCTORS
+    // ################################### */
+    AIBoat(int x, int y, int w, int h, String texture_path) {
+        super(x, y, w, h, texture_path);
+
+    }
+
+}
