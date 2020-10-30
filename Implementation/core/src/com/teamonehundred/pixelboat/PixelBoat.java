f65430fe835c3d5144f1a4a266e3fe9ea52378e0
wpw503 <wpw503@york.ac.uk>
2020-10-30 02:26:59 +0000

    fixing small lib errors

--- Implementation/core/src/com/teamonehundred/pixelboat/PixelBoat.java
+++ Implementation/core/src/com/teamonehundred/pixelboat/PixelBoat.java
@@ -41,5 +41,8 @@ public class PixelBoat extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
+
+        Gdx.app.exit();
+        System.exit(0);
     }
 }
