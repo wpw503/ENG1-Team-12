cd69334dcb0e9a949796dd3ebbb0fc56cc95b393
Umer Fakher <66469756+UmerFakher@users.noreply.github.com>
2020-11-14 16:42:34 +0000

    Updated Results Scene. Issue with new Scene.
    Issue Leg ends and new one starts before you can read the leg times from results screen.
    Perhaps have it so you press a button before next leg.

--- Implementation/core/src/com/teamonehundred/pixelboat/SceneResultsScreen.java
+++ Implementation/core/src/com/teamonehundred/pixelboat/SceneResultsScreen.java
@@ -1,5 +1,10 @@
 package com.teamonehundred.pixelboat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.List;
@@ -8,9 +13,15 @@ class SceneResultsScreen implements Scene {
     protected int scene_id = 4;
 
     protected List<Boat> boats;
+    protected BitmapFont font; // For text display - UF
+//    protected OrthographicCamera fill_camera;
 
     SceneResultsScreen() {
         boats = null;
+
+        // Initialise colour of Text Display Overlay - UF
+        font = new BitmapFont();
+        font.setColor(Color.BLUE);
     }
 
     // return 1 when you want to exit the results screen
@@ -36,6 +47,38 @@ class SceneResultsScreen implements Scene {
     }
 
     public void draw(SpriteBatch batch) {
+        Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//        batch.setProjectionMatrix(fill_camera.combined);
+        batch.begin();
+//
+        PlayerBoat thePlayerBoat = null;
+        //         your draw code
+        for (Boat b : boats){
+            if (b instanceof  PlayerBoat){
+                thePlayerBoat = (PlayerBoat) b;
+
+            }
+        }
+//
+        String label_text = thePlayerBoat.getName();
+        font.draw(batch, label_text, -thePlayerBoat.ui_bar_width / 2, 470 + thePlayerBoat.getSprite().getY());
+        batch.end();
+
+//        for (Boat b : boats) {
+//            if (b instanceof  PlayerBoat) {
+//                List<Long> legTimes = b.getLegTimes();
+//                float boatTime = legTimes.get(legTimes.size() - 1);
+//                String label_text = b.getName();//String.format("A boat (%s) ended race with time (ms) %d (%d ms was penalty)", b.getName(), boatTime, b.getTimeToAdd());
+//                //String.format(label_text, boatTime / 60000, boatTime / 1000 % 60)
+//
+//                // font.draw(batch, label_text,-thePlayerBoat.ui_bar_width /2,500 - (boats.indexOf(b)*10) + thePlayerBoat.getSprite().getY());
+////            drawTimeDisplay(batch, b, label, i, -((PlayerBoat) b).ui_bar_width / 2,
+////                    500 - (legtimes.size() * 20) + ((PlayerBoat) b).getSprite().getY());
+//                font.draw(batch, label_text, -thePlayerBoat.ui_bar_width / 2, 470 + thePlayerBoat.getSprite().getY());
+//            }
+//        }
     }
 
     public void resize(int width, int height) {
