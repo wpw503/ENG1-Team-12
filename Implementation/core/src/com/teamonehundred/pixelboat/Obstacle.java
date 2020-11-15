e87735605a37b8945a5d4ed3920932882f6885be
Umer Fakher <66469756+UmerFakher@users.noreply.github.com>
2020-11-15 18:56:56 +0000

    All Obstacle JavaDocs

--- Implementation/core/src/com/teamonehundred/pixelboat/Obstacle.java
+++ Implementation/core/src/com/teamonehundred/pixelboat/Obstacle.java
@@ -4,20 +4,52 @@ import com.badlogic.gdx.graphics.Texture;
 import com.teamonehundred.pixelboat.CollisionObject;
 import com.teamonehundred.pixelboat.MovableObject;
 
+/**
+ * Represents the obstacle as an abstract class that extends from movable object
+ * and implements the CollisionObject interface.
+ *
+ * @author James Frost
+ * @author William Walton
+ * JavaDoc by Umer Fakher
+ */
 abstract class Obstacle extends MovableObject implements CollisionObject {
     /* ################################### //
                   CONSTRUCTORS
     // ################################### */
 
+    /**
+     * A constructor for an Obstacle taking its position (x and y) and width and height.
+     * <p>
+     *
+     * Accepts Texture indirectly through file path. Integer for frame count not needed.
+     * @author James Frost
+     * @author William Walton
+     */
     Obstacle(int x, int y, int w, int h, String texture_path) {
         super(x, y, w, h, texture_path);
         //todo implement this
     }
 
+    /**
+     * A constructor for an Obstacle taking its position (x and y) and width and height.
+     * <p>
+     *
+     * Accepts Texture indirectly through file path. Integer for frame count needed.
+     * @author James Frost
+     * @author William Walton
+     */
     Obstacle(int x, int y, int w, int h, String texture_path, int frame_count){
         super(x, y, w, h, texture_path,frame_count);
     }
 
+    /**
+     *  A constructor for an Obstacle taking its position (x and y) and width and height.
+     * <p>
+     *
+     * Accepts Texture directly. Integer for frame count needed.
+     * @author James Frost
+     * @author William Walton
+     */
     Obstacle(int x, int y, int w, int h, Texture t, int frame_count){
         super(x, y, w, h, t ,frame_count);
     }
@@ -26,6 +58,12 @@ abstract class Obstacle extends MovableObject implements CollisionObject {
                     METHODS
     // ################################### */
 
+    /**
+     * Removes object from game screen once a boat has collided with it.
+     *
+     * @author James Frost
+     * @author William Walton
+     */
     public void hasCollided() {
         is_shown = false;
     }
--- Implementation/core/src/com/teamonehundred/pixelboat/ObstacleDuck.java
+++ Implementation/core/src/com/teamonehundred/pixelboat/ObstacleDuck.java
@@ -3,7 +3,25 @@ package com.teamonehundred.pixelboat;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Represents the simple duck obstacle.
+ *
+ * @author James Frost
+ * @author William Walton
+ * JavaDoc by Umer Fakher
+ */
 class ObstacleDuck extends Obstacle{
+
+    /**
+     * A constructor for an Obstacle taking its position (x and y).
+     * <p>
+     *
+     * Duck image is taken by default from C:\...\ENG1-Team-12\Implementation\core\assets.
+     * Random rotation is set.
+     *
+     * @author James Frost
+     * @author William Walton
+     */
     ObstacleDuck(int x, int y) {
         super(x, y, 30, 30, "obstacle_duck.png");
         // give each duck a random rotation
@@ -15,16 +33,28 @@ class ObstacleDuck extends Obstacle{
         rotation_speed = .2f;
     }
 
+    /**
+     * Updates position of duck obstacle and turns by 1 point.
+     */
     @Override
     public void updatePosition() {
         turn(1);
         super.updatePosition();
     }
 
+    /**
+     * Returns a new collision bounds object for the duck obstacle.
+     * <p>
+     *
+     * Creates a new collision bounds object representing the current position of this duck.
+     * See the collision bounds visualisation folder in assets for a visual representation.
+     *
+     * @return CollisionBounds of duck obstacle
+     * @author James Frost
+     * @author William Walton
+     */
     @Override
     public CollisionBounds getBounds() {
-        // create a new collision bounds object representing my current position
-        // see the collision bounds visualisation folder in assets for a visual representation
         CollisionBounds my_bounds = new CollisionBounds();
         Rectangle r1 = new Rectangle(
                 sprite.getX()+(0.09f*sprite.getWidth()),
