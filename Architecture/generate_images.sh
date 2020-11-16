b92765adcbc3c9704babfea7be7d19ec8c30be8a
wpw503 <wpw503@york.ac.uk>
2020-11-16 16:42:40 +0000

    more uml

--- Architecture/generate_images.sh
+++ Architecture/generate_images.sh
@@ -4,4 +4,8 @@ echo Converting to SVG
 plantuml -tsvg UML.txt
 echo Converting to PNG
 plantuml -tpng UML.txt
+echo Converting to SVG for Abstract
+plantuml -tsvg UML_abstract.txt
+echo Converting to PNG for Abstract
+plantuml -tpng UML_abstract.txt
 echo Done
