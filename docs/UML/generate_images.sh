546104c43e3b1610952cec523c7491d9f3173b78
wpw503 <wpw503@york.ac.uk>
2020-11-22 19:03:36 +0000

    arch1

--- docs/UML/generate_images.sh
+++ docs/UML/generate_images.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo Converting to SVG
-plantuml -tsvg UML.txt
-echo Converting to PNG
-plantuml -tpng UML.txt
-echo Converting to SVG for Abstract
-plantuml -tsvg UML_abstract.txt
-echo Converting to PNG for Abstract
-plantuml -tpng UML_abstract.txt
-echo Done
+for f in ./plantUML_source/*
+do
+  echo "Processing $f file..."
+  plantuml -tpng $f -o "../PNG/"
+  plantuml -tsvg $f -o "../SVG/"
+done
