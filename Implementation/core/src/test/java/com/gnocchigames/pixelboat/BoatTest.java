f929519f712c2dc63486761c1c35e524af9f519b
Adam Blanchet <akb561@york.ac.uk>
2021-01-26 14:59:45 +0100

    add initial unit test

---
+++ Implementation/core/src/test/java/com/gnocchigames/pixelboat/BoatTest.java
@@ -0,0 +1,18 @@
+package com.gnocchigames.pixelboat;
+
+import org.junit.jupiter.api.Test;
+
+
+import com.teamonehundred.pixelboat.entities.AIBoat;
+import com.teamonehundred.pixelboat.entities.Boat;
+import com.teamonehundred.pixelboat.entities.PlayerBoat;
+
+public class BoatTest {
+    
+    @Test
+    public void testBoat() {
+        System.out.println("test run");
+        //throw new Error();
+    }
+
+}
