ffdad50bba73d513c116728c9d13c7fa8b7de534
Adam <13720823+Frinksy@users.noreply.github.com>
2021-02-08 14:55:44 +0100

    Set documentation to be updated on separate repository (#18)

--- update-docs.sh
+++ update-docs.sh
@@ -1,27 +1,37 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 cd Implementation &&
 chmod +x gradlew &&
 echo Generating new javadocs &&
 ./gradlew javadoc &&
 echo Removing all javadocs from website &&
-rm -rf ../docs/* &&
+rm -rf ../../pixelboatWebsite/docs/* &&
 echo Copying javadocs to website &&
-cp -r core/build/docs/javadoc/* ../docs/  &&
+cp -r core/build/docs/javadoc/* ../../pixelboatWebsite/docs/  &&
 cd .. &&
 echo Generating UML images &&
 cd UML &&
 chmod +x generate_images.sh &&
 ./generate_images.sh &&
 cd .. &&
+echo Copying UML to website &&
+cp -r UML/* ../pixelboatWebsite/UML/ &&
+echo Moving to website folder &&
+cd ../pixelboatWebsite &&
 echo Set up Git &&
 git config --local user.email "13720823+Frinksy@users.noreply.github.com" &&
 git config --local user.name "Frinksy Workflow" &&
 echo Stage all changes &&
 git add docs/ &&
 git add UML/PNG/ && git add UML/SVG/ &&
-echo Commit changes &&
-git commit -m "Update docs via Github Workflow" &&
-echo Push changes &&
-git push &&
+echo Check for changes
+
+if git diff --staged --quiet ; then
+    echo No changes found
+else
+    echo Changes found, creating a commit &&
+    git commit -m "Update docs via Github Workflow" &&
+    echo Push changes &&
+    git push 
+fi
 echo Done!
