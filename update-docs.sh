5015c50b77ca7c8be912d8cdefe1ec8f89233e2c
Adam <13720823+Frinksy@users.noreply.github.com>
2021-02-08 18:48:39 +0100

    add test reports to documentation CI (#19)
    * add test reports to documentation CI
    
    * change name of last step

--- update-docs.sh
+++ update-docs.sh
@@ -2,6 +2,10 @@
 
 cd Implementation &&
 chmod +x gradlew &&
+
+#####################
+# Generate javadocs #
+#####################
 echo Generating new javadocs &&
 ./gradlew javadoc &&
 echo Removing all javadocs from website &&
@@ -9,6 +13,11 @@ rm -rf ../../pixelboatWebsite/docs/* &&
 echo Copying javadocs to website &&
 cp -r core/build/docs/javadoc/* ../../pixelboatWebsite/docs/  &&
 cd .. &&
+
+#########################
+# Generate UML diagrams #
+#########################
+
 echo Generating UML images &&
 cd UML &&
 chmod +x generate_images.sh &&
@@ -16,6 +25,25 @@ chmod +x generate_images.sh &&
 cd .. &&
 echo Copying UML to website &&
 cp -r UML/* ../pixelboatWebsite/UML/ &&
+
+
+
+
+#########################
+# Generate Test reports #
+#########################
+
+cd Implementation &&
+./gradlew test &&
+cd .. &&
+rm -rf ../pixelboatWebsite/reports &&
+mv Implementation/core/build/reports ../pixelboatWebsite/ &&
+
+
+################
+# Push Changes #
+################
+
 echo Moving to website folder &&
 cd ../pixelboatWebsite &&
 echo Set up Git &&
