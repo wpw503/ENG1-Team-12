6de8be92a96dbff9f5c6ccf5d414caad8300bbd4
kr337 <72796554+kr337@users.noreply.github.com>
2021-02-09 20:13:19 +0000

    website update

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -4,12 +4,33 @@ $(window).on('load', function(){
         openInfo(event, 'docs')
     })
 
+    $('#docbtn2').on('click', function(){
+        openInfo(event, 'docs2')
+    })
+
     $('#picbtn').on('click', function(){
         openInfo(event, 'pics')
     })
 
+
     $('#linkbtn').on('click', function(){
         openInfo(event, 'links')
+    })
+
+    $('#impbtn').on('click', function(){
+        openInfo(event, 'imp')
+    })
+
+    $('#testbtn').on('click', function(){
+        openInfo(event, 'test')
+    })
+
+    $('#cibtn').on('click', function(){
+        openInfo(event, 'ci')
+    })
+
+    $('#chngbtn').on('click', function(){
+        openInfo(event, 'chng')
     })
 
 })
@@ -21,12 +42,12 @@ function openInfo(evt, tabname) {
     // Get all elements with class="tablinks" and remove the class "active"
 
     $('.navlinks').removeClass('active');
-  
+
     // Show the current tab, and add an "active" class to the button that opened the tab
 
     var tempname = '#' + tabname;
 
     $(tempname).show();
-    
+
     evt.currentTarget.className += " active";
 }
